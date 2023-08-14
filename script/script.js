5173e5a603b254598a4c76c7154da4655dbb4843
Amr Shalaby <53496816+amr1234567@users.noreply.github.com>
2023-08-14 12:19:08 +0300

    small edits

--- script/script.js
+++ script/script.js
@@ -1,13 +1,6 @@
-let button = document.getElementsByClassName("submit-button")[0];
 let emailField = document.getElementById("email-text-area");
-let message = document.getElementsByClassName("success-message");
-let mainWindow = document.getElementsByClassName("container")[0];
 let labelInvalid = document.querySelector('.labels span');
-console.log(labelInvalid);
 var mailformat = /\w+@\w+.\w+/;
-
-// button.addEventListener('click', validate());
-
 
 function validate() {
     if (!emailField.value.match(mailformat)) {
